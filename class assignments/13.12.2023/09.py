106646e0eb884d2a4d87f9e3dc52f73016422b58
Niranjan Rajkumar <128575600+Niranjanr0682@users.noreply.github.com>
2024-01-07 12:22:37 +0530

    Update 09.py

--- class assignments/13.12.2023/09.py
+++ class assignments/13.12.2023/09.py
@@ -2,23 +2,32 @@
 # and perimeter.Then, create subclasses for specific shapes like rectangle and circle.
 
 import math
+
+
 class Shape:
     def area(self):
         pass
 
     def perimeter(self):
         pass
+
+
 class Rectangle(Shape):
     def __init__(self, length, width):
         self.length = length
         self.width = width
+
     def area(self):
         return self.length * self.width
+
     def perimeter(self):
         return 2 * (self.length + self.width)
+
+
 class Circle(Shape):
     def __init__(self, radius):
         self.radius = radius
+
     def area(self):
         return math.pi * self.radius ** 2
 
